ca229bf85dfc1573f47fbf0a50c0ad2fb54f58d7
Rakscreator <90820664+Rakscreator@users.noreply.github.com>
2023-01-17 21:43:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 function setup(){
-    canvas = createCanvas(500,500)
-    canvas.center()
+    canvas = createCanvas(550,500);
+    canvas.position(800,150);
+    
+    video = createCapture(VIDEO);
+    video.size(550,500)
+
+    posenet = ml5.poseNet(video,modelloaded);
+    posenet.on("pose",gotPoses);
+}
+function modelloaded(){
+    console.log("Posenet ACTIVATED");
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+    }
 }
